8c889c7d23b15a4c0546a6d408a83f6d1455c889
Abhijeet Kushwaha <abhijeetrkushwaha28@gmail.com>
2020-05-28 11:14:22 +0530

    weather app done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+require('dotenv').config()
+// console.log(process.env);
+
+app.use(express.static("public"));
+app.use(express.json());
+app.post("/api", async (req, res) => {
+//   console.log(req.body);
+  let data = req.body;
+  let apiKey = process.env.API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${data.place}&units=Metric&appid=${apiKey}`;
+//   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.log}&units=Metric&appid=`;
+  const response = await fetch(url);
+  const urlData = await response.json();
+  res.json(urlData);
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening to port ${PORT}`));
---
+++ public/script.js
@@ -0,0 +1,77 @@
+window.onload = function (){
+    let cityData = document.querySelector(".city-data");
+    let search = document.querySelector(".search");
+    let display = document.querySelector('.display');
+     async function postData(place) {
+       const data = {
+         place: place,
+       };
+       const object = {
+         method: "POST",
+         headers: {
+           "Content-Type": "application/json",
+         },
+         body: JSON.stringify(data),
+       };
+       const response = await fetch("/api", object);
+       const resData = await response.json();
+       if(resData.cod == '404'){
+           alert('Enter valid city name')
+           return;
+       }
+    //    console.log(resData);
+    display.style.display = 'block'
+       let iconImg = document.querySelector(".icon-img");
+       let dataDescription = document.querySelector(".data-description");
+       let maxTemp = document.querySelector(".max-temp");
+       let minTemp = document.querySelector(".min-temp");
+       let mainTempData = document.querySelector(".main-temp-data");
+       let placeData = document.querySelector(".place-data");
+       let feelsLike = document.querySelector(".feels-like");
+       let iconcode = resData.weather[0].icon
+       iconImg.src = `http://openweathermap.org/img/wn/${iconcode}.png`;
+       dataDescription.innerHTML = resData.weather[0].description;
+       maxTemp.innerHTML = resData.main.temp_max;
+       minTemp.innerHTML = resData.main.temp_min;
+       mainTempData.innerHTML = resData.main.temp;
+       feelsLike.innerHTML = resData.main.feels_like;
+    //    let upperCasePlace = place.toUppercase()
+       placeData.innerHTML = resData.name;
+       // console.log(resData);
+     }
+
+
+    search.addEventListener('click', (e)=>{
+        e.preventDefault();
+        if(cityData.value.trim().length > 0){
+            postData(cityData.value);
+            cityData.value = '';
+        }else{
+            alert('Enter city name')
+        }
+    })
+   
+    
+}
+
+// if ("geolocation" in navigator) {
+//   console.log("geolocation is available");
+//   navigator.geolocation.getCurrentPosition(async (position) => {
+//     const lat = position.coords.latitude;
+//     const log = position.coords.longitude;
+//     const data = { lat, log };
+//     const object = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     };
+//     const response = await fetch("/api", object);
+//     const resData = await response.json();
+//     console.log(resData);
+//     console.log(resData.main.temp);
+//   });
+// } else {
+//   console.log("geolocation not available");
+// }
